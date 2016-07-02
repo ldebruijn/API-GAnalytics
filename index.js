207a21fc625aa59645b6407c027718fde630a05d
Lars de Bruijn <larsdebruijn12@gmail.com>
2016-07-02 23:47:37 +0200

    Added support for exceptions

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const General = require('./lib/models/general');
 const Session = require('./lib/models/session');
 const Event = require('./lib/models/event');
 const Pageview = require('./lib/models/pageview');
+const Exception = require('./lib/models/exception');
+const RequestBody = require('./lib/models/requestbody');
 const Request = require('./lib/request');
 
 module.exports = function(trackingId, options) {
@@ -25,8 +27,14 @@ module.exports = function(trackingId, options) {
         let session = new Session(req, options);
         let event = new Event(req, res);
         let pageview = new Pageview(req);
+        let exception = new Exception(err);
 
-        let request = new Request(req, general, session, event, pageview);
+        let requestBody = new RequestBody(req, general, session)
+            .setEvent(event)
+            .setPageview(pageview)
+            .setException(exception);
+
+        let request = new Request(requestBody);
         request.send();
         
         next(err);
