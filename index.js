d45cf63a26ee55fa110936252a5dc70b1fc9fc7b
Lars de Bruijn <ldebruijn@bol.com>
2016-10-13 18:04:39 +0200

    Added exception event hook

--- index.js
+++ index.js
@@ -38,5 +38,34 @@ module.exports = function(trackingId, options) {
     }
 };
 
+module.exports.exceptions = function(trackingId, options) {
+    if (!_.isString(trackingId)) {
+        throw new Error('Google Analytics Tracking ID must be a string');
+    }
+
+    options = options || {};
+    options.userId = options.userId || [];
+    options.debug = options.debug || false;
+
+    const general = new General(trackingId, options);
+
+    return (req, res, next, err) => {
+        let session = new Session(req, options);
+        let event = new Event(req, res);
+        let pageview = new Pageview(req);
+        let exception = new Exception(err);
+
+        let requestBody = new RequestBody(req, general, session)
+            .setEvent(event)
+            .setPageview(pageview)
+            .setException(exception);
+
+        let request = new Request(requestBody, options.debug);
+        request.send();
+
+        next();
+    }
+}
+
 // @TODO: Error handling middleware.
 
